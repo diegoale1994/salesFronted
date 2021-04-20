53c5872436c8a1426415da07b5429fa05c125daf
Diego Alejandro <diego.fc.1@hotmail.com>
2021-04-19 22:30:01 -0500

    initialCommit

---
+++ pages/nuevacuenta.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { Layout } from '../components/Layout';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+const nuevacuenta = () => {
+
+
+    //validacion de formulario
+
+    const formik = useFormik({
+        initialValues: {
+            nombre: '',
+            apellido: 'Franco',
+            email: '',
+            password: ''
+        },
+        onSubmit: valores => {
+            console.log('enviando');
+            console.log(valores)
+        }
+    });
+
+    return (
+        <>
+            <Layout>
+                <h1 className="text-center text-2xl text-white font-light">Crear nueva cuenta</h1>
+                <div className="flex justify-center mt-5">
+                    <div className="w-full max-w-sm">
+                        <form className="bg-white rounded shadow-md px-8 pt-6 pb-8  mb-4" onSubmit={formik.handleSubmit}>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
+                                    Nombre
+                                </label>
+                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="nombre" type="text" placeholder="Nombre" value={formik.values.nombre} onChange={formik.handleChange} />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellido">
+                                    Apellido
+                                </label>
+                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="apellido" type="text" placeholder="Apellido" value={formik.values.apellido} onChange={formik.handleChange} />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                                    Email
+                                </label>
+                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="email" type="email" placeholder="Email" value={formik.values.email} onChange={formik.handleChange} />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                                    Password
+                                </label>
+                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="password" type="password" placeholder="Password" value={formik.values.password} onChange={formik.handleChange} />
+                            </div>
+                            <input type="submit" className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900"
+                                value="Crear Cuenta" />
+                        </form>
+                    </div>
+                </div>
+            </Layout>
+        </>
+    )
+}
+
+export default nuevacuenta;
